c9a55acf97ebd619b8067f8453b6b54d1fab10ad
Andrew Frahn <andrew@emmertex.com>
2024-01-07 09:33:58 +1100

    Better display of ToD filter

--- chore_app/templatetags/extra_filters.py
+++ chore_app/templatetags/extra_filters.py
@@ -2,9 +2,16 @@ from django import template
 
 register = template.Library()
 
-@register.filter()
+@register.filter(name='halve')
 def halve(value):
     try:
         return str(int(value / 2))
+    except (ValueError, TypeError):
+        return str(int(value))
+    
+@register.filter(name='abs_filter')
+def abs_filter(value):
+    try:
+        return str(int(abs(value)))
     except (ValueError, TypeError):
         return str(int(value))
