923bcef204f66f381c50900c54f2049096d092dc
BuildTools <unconfigured@null.spigotmc.org>
2024-01-27 12:42:37 +0000

    Fixes with daily action

--- chore_app/cron.py
+++ chore_app/cron.py
@@ -48,10 +48,8 @@ def nightly_action(approver=None):
     # Process each child's incomplete chores penalties and bonusses
     for child in children:
         try:
-            points_penalty = incomplete_chore_penalty(
-                approver=approver, child=child, children=children, settings=settings)
-            apply_daily_bonus(approver=approver, child=child,
-                              incomplete_chores_sum=points_penalty, settings=settings)
+            incomplete_chore_penalty(approver=approver, child=child, settings=settings)
+            apply_daily_bonus(approver=approver, child=child, settings=settings)
         except Exception as e:
             logging.error(e)
             raise
@@ -78,7 +76,7 @@ def nightly_action(approver=None):
 def apply_leaderboard_scoring(approver, children, settings):
 
     # Sum all the points each child earned from chores today
-    chore_points = models.PointLog.objects.filter(date_recorded__date=datetime.date.today()).exclude(
+    chore_points = models.PointLog.objects.filter(date_recorded__date=datetime.now().date()).exclude(
         chore='').values('user', 'user__username').annotate(total_points=Sum('points_change')).order_by('-total_points')
 
     # Create text for the Leaderboard
@@ -116,11 +114,11 @@ def apply_leaderboard_scoring(approver, children, settings):
         for i in range(3, len(chore_points)):
             models.PointLog.objects.create(user=models.User.objects.get(
                 pk=chore_points[i]['user']), points_change=0, reason=leaderboard_text, approver=approver)
+    return
 
 
 # Daily Bonus
-def apply_daily_bonus(approver, child, incomplete_chores_sum, settings):
-    child.points_balance += incomplete_chores_sum
+def apply_daily_bonus(approver, child, settings):
     points_balance = child.points_balance
     pocket_money = child.pocket_money
 
@@ -137,23 +135,31 @@ def apply_daily_bonus(approver, child, incomplete_chores_sum, settings):
 
     models.PointLog.objects.create(user=child, points_change=points_balance - child.points_balance,
                                    penalty=0, reason='Daily Points', chore='', approver=approver)
-
+    return
 
 # Incomplete Chores Penalty
-def incomplete_chore_penalty(approver, child, children, settings):
-    available_chores = models.Chore.objects.filter(available=True, persistent=False)
+def incomplete_chore_penalty(approver, child, settings):
+    available_chores = models.Chore.objects.filter(available=True)
 
     if available_chores and settings['incomplete_chores_penalty'] > 0:
         complete_chores = models.ChoreClaim.objects.filter(approved__gt=0)
+        complete_chores_sum = sum(chore.approved for chore in complete_chores)
+
         completed_by_child = models.ChoreClaim.objects.filter(user=child, approved__gt=0)
-        
-        incomplete_chores_sum = -sum(chore.points for chore in available_chores)
-        complete_chores_sum = sum(chore.points for chore in complete_chores)
         completed_by_child_sum = sum(chore.points for chore in completed_by_child)
+
+        penalised_chores = models.ChoreClaim.objects.filter(approved__lt=0)
+        penalised_chores_sum = sum(chore.approved for chore in penalised_chores)
+        
+        incomplete_chores_sum = 0
+        completed_by_child_ids = completed_by_child.values_list('chore_id', flat=True)
+        for chore in available_chores:
+            if chore.id not in completed_by_child_ids:
+                incomplete_chores_sum += chore.points
 
         penalty_multiplier = settings['incomplete_chores_penalty'] / 100
 
-        incomplete_chores_penalty = penalty_multiplier * incomplete_chores_sum * (1 - completed_by_child_sum / complete_chores_sum)
+        incomplete_chores_penalty = penalty_multiplier * (incomplete_chores_sum + penalised_chores_sum) * (1 - completed_by_child_sum / complete_chores_sum)
 
         models.PointLog.objects.create(
             user=child, 
@@ -163,9 +169,11 @@ def incomplete_chore_penalty(approver, child, children, settings):
             chore='', 
             approver=approver
         )
-        return incomplete_chores_penalty
 
-    return 0
+        updated_points = child.points_balance - incomplete_chores_penalty
+        models.User.objects.filter(pk=child.pk).update(points_balance=updated_points)
+        
+    return
 
 # Automatically approve pending claimed chores
 def auto_approve(approver, settings):
@@ -175,9 +183,11 @@ def auto_approve(approver, settings):
         
         for chore_claim in unapproved_chores:
             views.approve_chore_claim(None, chore_claim.pk, penalty, auto=True)
+    return
 
 # Reset Daily Chores to Available, and clear claimed chores
 def reset_daily_chores():
     models.ChoreClaim.objects.filter(approved__gt=0).delete()
     models.ChoreClaim.objects.filter(approved__lt=0).delete()
     models.Chore.objects.filter(daily=True).update(available=True)
+    return
