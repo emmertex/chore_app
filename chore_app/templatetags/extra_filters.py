5e35a194619b4a6522a908d2423458aba6718569
Andrew Frahn <andrew@emmertex.com>
2025-10-25 09:06:04 +1100

    bug in filter

--- chore_app/templatetags/extra_filters.py
+++ chore_app/templatetags/extra_filters.py
@@ -24,32 +24,6 @@ def abs_filter(value):
     except (ValueError, TypeError):
         return str(int(value))
 
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='halve')
-def halve(value):
-    try:
-        return str(int(value / 2))
-    except (ValueError, TypeError):
-        return str(int(value))
-
-@register.filter(name='fifth')
-def fifth(value):
-    try:
-        return str(int(value / 5))
-    except (ValueError, TypeError):
-        return str(int(value))
-
-@register.filter(name='abs_filter')
-def abs_filter(value):
-    try:
-        return str(int(abs(value)))
-    except (ValueError, TypeError):
-        return str(int(value))
-
 @register.filter(name='format_username')
 def format_username(value):
     """Format username with first letter capitalized and rest lowercase"""
